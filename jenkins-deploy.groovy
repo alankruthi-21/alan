a22eeaa40b67771607b3ed532175886ab6aa378c
alankruthi-21 <2215316621@gitam.in>
2022-05-18 19:43:06 +0530

    de0

--- jenkins-deploy.groovy
+++ jenkins-deploy.groovy
@@ -1,6 +1,6 @@
 pipeline{
     agent any
-   parameters {
+   parameters{
     string(name: 'BRANCH_NAME', defaultValue: '', description: '')
     string(name: 'BUILD_NUM', defaultValue: '', description: '')
     string(name: 'SERVER_IP', defaultValue: '', description: '')
@@ -9,7 +9,7 @@ pipeline{
     stages{
        
         stage("deploy to multiple servers") {
-            steps {
+            steps{
                
                 sh '''
                 aws s3 cp s3://alankruthiart/application2/${BUILD_NUM}/hello-${BUILD_NUM}.war .
@@ -19,8 +19,8 @@ pipeline{
                  do
                  echo $i
                  
-                 "scp -o StrictHostKeyChecking=no -i /tmp/alankruthi21.pem hello-${BUILD_NUM}.war ec2-user@$i:/tmp"
-                 "ssh -o StrictHostKeyChecking=no -i /tmp/alankruthi21.pem ec2-user@$i "sudo cp /tmp/hello-${BUILD_NUM}.war /var/lib/tomcat/webapps""
+                 scp -o StrictHostKeyChecking=no -i /tmp/alankruthi21.pem hello-${BUILD_NUM}.war ec2-user@$i:/tmp
+                 ssh -i /tmp/alankruthi21.pem ec2-user@$i "sudo cp /tmp/hello-${BUILD_NUM}.war /var/lib/tomcat/webapps"
                done
                  '''
             }
